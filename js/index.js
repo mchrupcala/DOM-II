bf4a9322467c549a85f346f5dfc7e15f0a6db571
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-08-06 17:15:09 -0400

    Added resize, scroll, and double-click events to my Javascript.

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ busImg.addEventListener('load', (e) => {
 })
 
 
-//Should be -> hover "Welcome to the Fun Bus" and text zooms away. Actually, will only respond if you click. Why?
+//Hover "Welcome to the Fun Bus" and text zooms away.
 const h2Fun = document.querySelector('.intro h2');
 
 h2Fun.addEventListener('mouseover', (event) => {
@@ -20,8 +20,52 @@ h2Fun.addEventListener('mouseover', (event) => {
 
 // Color the nav bar a random color whenever any key is hit.
 
-const navLink = document.getElementsByClassName('nav-link');
+const navLink = document.querySelectorAll('a');
 
-navLink.addEventListener('keydown', (e) => {
-    navLink.style.color = 'red';
+var colors = ['red', 'purple', 'blue', 'green', 'black', 'tan', 'skyblue', 'magenta', 'yellow', 'orange'];
+
+console.log(navLink);
+navLink.forEach((attrs) => {
+    window.addEventListener('keydown', (e) => {
+        var random_color = colors[Math.floor(Math.random() * colors.length)];
+        attrs.style.color = random_color;
+    });
+})
+
+
+// On resize, change all text to comic sans, and back again.
+
+const text = document.querySelectorAll('p');
+var count = 1;
+function resized() {
+    count++;
+}
+window.onresize = resized;
+
+text.forEach((attrs) => {
+    window.addEventListener('resize', (e) => {
+
+        if (count%2 == 0) {
+            attrs.style.fontFamily = 'Comic Sans MS';
+        }
+        else {
+            attrs.style.fontFamily = 'Times New Roman';
+        }
+    });
+})
+
+// On scroll, all images should bounce.
+
+const imgs = document.querySelectorAll('img');
+imgs.forEach((attrs) => {
+    window.addEventListener('scroll', (e) => {
+        attrs.setAttribute('class', 'bounce animated');
+    })
+})
+
+//Double-click "Adventure Awaits" and the text flies away.
+const go = document.querySelector('.inverse-content .text-content h2');
+
+go.addEventListener('dblclick', (event) => {
+    event.target.setAttribute('class', 'lightSpeedOut animated')
 })
